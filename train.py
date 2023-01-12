284b797ee0680b148bdbd4a1fafb99561d7d6296
dberenbaum <dave@iterative.ai>
2023-01-12 15:49:52 -0500

    tensorflow->lightning

--- train.py
+++ train.py
@@ -1,9 +1,9 @@
 import os
 import numpy as np
-import tensorflow as tf
-import tensorflow_addons as tfa
-from tensorflow import keras
-from dvclive.keras import DVCLiveCallback
+import torch
+import pytorch_lightning as pl
+import torchmetrics
+from dvclive.lightning import DVCLiveLogger
 
 from params import *
 
@@ -18,20 +18,20 @@ np.random.seed(seed)
 np.random.shuffle(lines)
 for line in lines[: min(num_samples, len(lines) - 1)]:
     input_text, target_text, _ = line.split("\t")
-    # We use "tab" as the "start sequence" character
-    # for the targets, and "\n" as "end sequence" character.
-    target_text = "\t" + target_text + "\n"
-    input_texts.append(input_text)
-    target_texts.append(target_text)
     for char in input_text:
         if char not in input_characters:
             input_characters.add(char)
     for char in target_text:
         if char not in target_characters:
             target_characters.add(char)
+    # We use "tab" as the "start sequence" character
+    # for the targets, and "\n" as "end sequence" character.
+    target_text = "\t" + target_text + "\n"
+    input_texts.append(input_text)
+    target_texts.append(target_text)
 
 input_characters = sorted(list(input_characters))
-target_characters = sorted(list(target_characters))
+target_characters = sorted(list(target_characters)) + ["\t", "\n"]
 num_encoder_tokens = len(input_characters)
 num_decoder_tokens = len(target_characters)
 max_encoder_seq_length = max([len(txt) for txt in input_texts])
@@ -47,86 +47,119 @@ input_token_index = dict([(char, i) for i, char in enumerate(input_characters)])
 target_token_index = dict([(char, i) for i, char in enumerate(target_characters)])
 
 encoder_input_data = np.zeros(
-    (len(input_texts), max_encoder_seq_length, num_encoder_tokens), dtype="float32"
+    (len(input_texts), max_encoder_seq_length), dtype=int
 )
 decoder_input_data = np.zeros(
-    (len(input_texts), max_decoder_seq_length, num_decoder_tokens), dtype="float32"
+    (len(input_texts), max_decoder_seq_length), dtype=int
 )
 decoder_target_data = np.zeros(
-    (len(input_texts), max_decoder_seq_length, num_decoder_tokens), dtype="float32"
+    (len(input_texts), max_decoder_seq_length), dtype=int
 )
 
 for i, (input_text, target_text) in enumerate(zip(input_texts, target_texts)):
     for t, char in enumerate(input_text):
-        encoder_input_data[i, t, input_token_index[char]] = 1.0
-    encoder_input_data[i, t + 1 :, input_token_index[" "]] = 1.0
+        encoder_input_data[i, t] = input_token_index[char]
+    encoder_input_data[i, t + 1 :] = input_token_index[" "]
     for t, char in enumerate(target_text):
         # decoder_target_data is ahead of decoder_input_data by one timestep
-        decoder_input_data[i, t, target_token_index[char]] = 1.0
+        decoder_input_data[i, t] = target_token_index[char]
         if t > 0:
             # decoder_target_data will be ahead by one timestep
             # and will not include the start character.
-            decoder_target_data[i, t - 1, target_token_index[char]] = 1.0
-    decoder_input_data[i, t + 1 :, target_token_index[" "]] = 1.0
-    decoder_target_data[i, t:, target_token_index[" "]] = 1.0
+            decoder_target_data[i, t - 1] = target_token_index[char]
+    decoder_input_data[i, t + 1 :] = target_token_index[" "]
+    decoder_target_data[i, t:] = target_token_index[" "]
 
 
-# Define an input sequence and process it.
-encoder_inputs = keras.Input(shape=(None, num_encoder_tokens))
-encoder = keras.layers.LSTM(latent_dim, return_state=True)
-encoder_outputs, state_h, state_c = encoder(encoder_inputs)
+# Define the model 
+class LSTMSeqToSeq(pl.LightningModule):
+    def __init__(self):
+        super().__init__()
+        self.encoder_embedding = torch.nn.Embedding(num_encoder_tokens, latent_dim)
+        self.encoder = torch.nn.LSTM(latent_dim, latent_dim, batch_first=True)
+        self.decoder_embedding = torch.nn.Embedding(num_decoder_tokens,
+                latent_dim)
+        self.decoder = torch.nn.LSTM(latent_dim, latent_dim, batch_first=True)
 
-# We discard `encoder_outputs` and only keep the states.
-encoder_states = [state_h, state_c]
+        self.out = torch.nn.Linear(latent_dim, num_decoder_tokens)
+        self.acc = torchmetrics.classification.MulticlassAccuracy(
+                num_decoder_tokens, average="micro")
 
-# Set up the decoder, using `encoder_states` as initial state.
-decoder_inputs = keras.Input(shape=(None, num_decoder_tokens))
+    def forward(self, x_encoder, x_decoder):
+        encoder_embedded = self.encoder_embedding(x_encoder)
+        encoder_outputs, (state_h, state_c) = self.encoder(encoder_embedded)
+        decoder_embedded = self.decoder_embedding(x_decoder)
+        # We discard `encoder_outputs` and only keep the states.
+        decoder_outputs, (_, _) = self.decoder(decoder_embedded, (state_h, state_c))
+        out = self.out(decoder_outputs)
+        return out
 
-# We set up our decoder to return full output sequences,
-# and to return internal states as well. We don't use the
-# return states in the training model, but we will use them in inference.
-decoder_lstm = keras.layers.LSTM(latent_dim, return_sequences=True, return_state=True)
-decoder_outputs, _, _ = decoder_lstm(decoder_inputs, initial_state=encoder_states)
-decoder_dense = keras.layers.Dense(num_decoder_tokens, activation="softmax")
-decoder_outputs = decoder_dense(decoder_outputs)
+    def training_step(self, batch, batch_idx):
+        (x_encoder, x_decoder), y = batch
+        out = self(x_encoder, x_decoder)
+        # Reshape each step
+        y = y.flatten()
+        out = out.flatten(end_dim=1)
+        # Log metrics
+        loss = torch.nn.functional.cross_entropy(out, y)
+        acc = self.acc(out, y)
+        self.log("train_loss", loss, on_step=True, on_epoch=True)
+        self.log("train_acc", acc, on_step=True, on_epoch=True)
+        return loss
 
-# Define the model that will turn
-# `encoder_input_data` & `decoder_input_data` into `decoder_target_data`
-weights_path = "model/weights.hdf5"
-os.makedirs("model", exist_ok=True)
-try:
-    model.load_weights(weights_path)
-except:
-    print(f"Unable to load weights from {weights_path}. Compiling new model.")
-    model = keras.Model([encoder_inputs, decoder_inputs], decoder_outputs)
-    optimizer = keras.optimizers.RMSprop(lr=lr)
-    model.compile(
-        optimizer="rmsprop",
-        loss="categorical_crossentropy",
-        metrics=["accuracy"]
-    )
+    def validation_step(self, batch, batch_idx):
+        (x_encoder, x_decoder), y = batch
+        out = self(x_encoder, x_decoder)
+        # Reshape each step
+        y = y.flatten()
+        out = out.flatten(end_dim=1)
+        # Log metrics
+        loss = torch.nn.functional.cross_entropy(out, y)
+        acc = self.acc(out, y)
+        self.log("val_loss", loss, on_step=True, on_epoch=True)
+        self.log("val_acc", acc, on_step=True, on_epoch=True)
+
+    def configure_optimizers(self):
+        optimizer = torch.optim.RMSprop(self.parameters(), lr=lr)
+        return optimizer
 
 
-metric = "val_accuracy"
-live = DvcLiveCallback(path="results", report=None, resume=True)
-checkpoint = keras.callbacks.ModelCheckpoint(
-   weights_path,
-   save_best_only=True,
-   save_weights_only=True,
-   verbose=True,
-   monitor=metric)
-reduce_lr = keras.callbacks.ReduceLROnPlateau(monitor=metric, 
-                                              patience=3,
-                                              verbose=True)
-early_stop = keras.callbacks.EarlyStopping(monitor=metric,
-                                           patience=10,
-                                           verbose=True)
-time_stop = tfa.callbacks.TimeStopping(seconds=3600, verbose=1)
-hist = model.fit(
-    [encoder_input_data, decoder_input_data],
-    decoder_target_data,
-    batch_size=batch_size,
-    epochs=100000,
-    validation_split=0.2,
-    callbacks=[live, checkpoint, reduce_lr, early_stop, time_stop]
-)
+arch = LSTMSeqToSeq()
+
+# load the data
+class CustomDataset(torch.utils.data.Dataset):
+    def __init__(self, encoder_input_data, decoder_input_data,
+            decoder_target_data):
+        self.encoder_input_data = encoder_input_data
+        self.decoder_input_data = decoder_input_data
+        self.decoder_target_data = decoder_target_data
+
+    def __len__(self):
+        return len(self.encoder_input_data)
+
+    def __getitem__(self, idx):
+        return (self.encoder_input_data[idx], self.decoder_input_data[idx]), \
+               self.decoder_target_data[idx]
+
+
+combined_data = CustomDataset(encoder_input_data, decoder_input_data, decoder_target_data)
+train_len = int(len(combined_data)*0.8)
+val_len = len(combined_data) - train_len
+train, val = torch.utils.data.random_split(combined_data, [train_len, val_len],
+        generator=torch.Generator().manual_seed(seed))
+train_loader = torch.utils.data.DataLoader(train, batch_size=batch_size)
+val_loader = torch.utils.data.DataLoader(val, batch_size=batch_size)
+
+live = DVCLiveLogger(dir="results", report=None)
+checkpoint = pl.callbacks.ModelCheckpoint(
+        dirpath="model",
+        filename="model",
+        monitor="val_acc",
+        mode="max",
+        save_weights_only=True, every_n_epochs=1)
+timer = pl.callbacks.Timer(duration="00:01:00:00")
+
+trainer = pl.Trainer(max_epochs=-1, logger=[live],
+                     callbacks=[timer, checkpoint])
+trainer.fit(model=arch, train_dataloaders=train_loader,
+        val_dataloaders=val_loader)
