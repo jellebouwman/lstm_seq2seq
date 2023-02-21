e8a847e2749f23818f85f38bbfac90de8c33ef64
dberenbaum <dave@iterative.ai>
2023-02-21 15:57:10 -0500

    modify params for quick training

--- params.py
+++ params.py
@@ -1,6 +1,6 @@
 batch_size = 512  # Batch size for training.
-latent_dim = 64  # Latent dimensionality of the encoding space.
-num_samples = 100000 # Number of samples to train on.
+latent_dim = 32  # Latent dimensionality of the encoding space.
+num_samples = 10000 # Number of samples to train on.
 lr = 0.01 # Learning rate for optimizer.
 seed = 423 # Random seed.
 duration = "00:00:30:00"
